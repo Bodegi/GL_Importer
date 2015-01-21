a62961706b8694c1e3d59d0674515f89afb93eb5
Bodegi <Bodegigaming@gmail.com>
2015-01-21 18:10:49 -0500

    Config Helper
    Added confighelper class

---
+++ GL_Importer/GL_Importer/ConfigHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace GL_Importer
+{
+    class ConfigHelper
+    {
+        public int Date { get; set; }
+        public int seg1 { get; set; }
+        public int seg2 { get; set; }
+        public int seg3 { get; set; }
+        public int Amount { get; set; }
+        public int lineItem { get; set; }
+
+        public ConfigHelper()
+        {
+                Date = Int32.Parse(ConfigurationManager.AppSettings["Date"]);
+                seg1 = Int32.Parse(ConfigurationManager.AppSettings["Seg1"]);
+                seg2 = Int32.Parse(ConfigurationManager.AppSettings["Seg2"]);
+                seg3 = Int32.Parse(ConfigurationManager.AppSettings["Seg3"]);
+                Amount = Int32.Parse(ConfigurationManager.AppSettings["Amount"]);
+                lineItem = Int32.Parse(ConfigurationManager.AppSettings["lineItem"]);
+        }
+ 
+    }
+}
